7662a66ecc6b219956ae075b94a403a9385b3eeb
Rahma Aulia Mangundap <rahmamangundap@gmail.com>
2024-11-15 09:35:50 +0700

    Perbaikan 1 route profile

--- resources/views/favorites/index.blade.php
+++ resources/views/favorites/index.blade.php
@@ -26,7 +26,7 @@
     <div class="bg-white p-6 rounded-lg shadow-md w-full lg:w-2/3 border-4 border-blue-800">
         <div class="flex justify-between items-center mb-4">
             <div class="text-sm text-gray-500">
-            <a href="{{ route('profile.favorites') }}" class="text-orange-500 hover:underline">Profil</a> &gt; Tersimpan
+            <a href="{{ route('profile.show') }}" class="text-orange-500 hover:underline">Profil</a> &gt; Tersimpan
             </div>
         </div>
         <h1 class="text-2xl font-bold mb-4">Tersimpan</h1>
